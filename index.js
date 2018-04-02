1ba574d1eddae3f298692ccaa164ad33493a0ae1
Jim Pick <jim@backblend.com>
2018-04-02 14:59:48 -0700

    Add one possible fix

--- index.js
+++ index.js
@@ -584,10 +584,8 @@ HyperDB.prototype._ready = function (cb) {
       if (err) return done(err)
 
       self._localWriter = self._writers[self.feeds.indexOf(self.local)]
-      console.log('Jim1', self.local.key.toString('hex'), self.local.length)
-      console.log('Jim1a', self._localWriter._feed.key.toString('hex'))
+      if (self._contentStorage) self._localWriter._ensureContentFeed(null)
       self._localWriter.head(function (err) {
-        console.log('Jim2')
         if (err) return done(err)
         if (!self._contentStorage) return done(null)
 
@@ -760,7 +758,6 @@ Writer.prototype.get = function (seq, cb) {
 
 Writer.prototype.head = function (cb) {
   var len = this.length()
-  console.log('Jim writer head', len)
   if (!len) return process.nextTick(cb, null, null, this._id)
   this.get(len - 1, cb)
 }
@@ -822,9 +819,7 @@ Writer.prototype._addWriters = function (head, cb) {
 }
 
 Writer.prototype._ensureContentFeed = function (key) {
-  console.log('Jim _ensureContentFeed a', key)
   if (this._contentFeed) return
-  console.log('Jim _ensureContentFeed b', key)
 
   var self = this
   var secretKey = null
--- test/content-feeds.js
+++ test/content-feeds.js
@@ -3,7 +3,6 @@ var rimraf = require('rimraf')
 var create = require('./helpers/create')
 var replicate = require('./helpers/replicate')
 
-/*
 tape('basic content feed', function (t) {
   var db = create.one(null, {contentFeed: true, valueEncoding: 'json'})
 
@@ -44,43 +43,29 @@ tape('replicating content feeds', function (t) {
     })
   })
 })
-*/
 
-tape('replicating content feeds, reopen', function (t) {
+tape('reopen content', function (t) {
   rimraf.sync('./db-1')
-  rimraf.sync('./db-2')
   var db = create.one(null, {
     contentFeed: true,
     storage: './db-1'
   })
   db.put('hello', 'world', function () {
-    var clone = create.one(db.key, {
-      contentFeed: true,
-      storage: './db-2'
-    })
     db.localContent.append('data', function () {
-      replicate(db, clone, function () {
-        clone.get('hello', function (err, node) {
+      db.get('hello', function (err, node) {
+        t.error(err, 'no error')
+        t.same(node.value, 'world')
+        db.put('hello', {start: 0, end: 1}, function (err) {
           t.error(err, 'no error')
-          t.same(node.value, 'world')
-          clone.put('hello', {start: 0, end: 1}, function (err) {
-            t.error(err, 'no error')
-            clone.contentFeeds[node.feed].get(0, function (err, buf) {
-              t.error(err, 'no error')
-              t.same(buf, Buffer.from('data'))
-              // Reopen
-              console.log('Jim reopening')
-              var clone2 = create.one(null, {
-                storage: './db-2',
-                // storage: clone._storage,
-                contentFeed: true
-              })
-              clone2.ready(() => {
-                console.log('Jim2 writable', clone2.localContent.writable)
-              })
-              t.end()
-            })
+          // Reopen, re-using storage
+          var dbReopened = create.one(null, {
+            storage: './db-1',
+            contentFeed: true
           })
+          dbReopened.ready(() => {
+            t.ok(dbReopened.localContent.writable)
+          })
+          t.end()
         })
       })
     })
